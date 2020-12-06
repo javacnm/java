0ae048e16be6cc21678fe401467209d1248bf384
Noor <noormahammad@users.noreply.github.com>
2020-12-06 12:09:18 -0500

    Hello world program and .gitignore file added

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+   Compiling a Java Program:    (this creates an output file with ".class" extension, for example, HelloWorld.class)
+   -------------------------
+   > javac HelloWorld.java    
+
+   Executing(Running) a Java Program: 
+   -----------------------------------
+   > java HelloWorld
+
+*/
+
+public class HelloWorld {
+   public static void main(String[] args) {   
+      // Prints "Hello, World" in console
+      System.out.println("Hello, World");
+   }
+}
